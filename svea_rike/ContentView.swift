e84aac1b86e8e782e5a4bba1e37cd5c8af7c94c3
Karl Söderberg <karl.soderberg@gmail.com>
2020-11-23 22:15:16 +0100

    Initial Commit

---
+++ svea_rike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  svea_rike
+//
+//  Created by Karl Söderberg on 2020-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ svea_rike/svea_rikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  svea_rikeApp.swift
+//  svea_rike
+//
+//  Created by Karl Söderberg on 2020-11-23.
+//
+
+import SwiftUI
+
+@main
+struct svea_rikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
