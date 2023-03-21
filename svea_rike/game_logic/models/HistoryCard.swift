aaa19484c44ce3ef72f6f42282e930409fb0b27f
Karl Söderberg <karl.soderberg@shortcut.io>
2023-03-21 22:33:30 +0100

    Added all cards as enum

--- svea_rike/game_logic/models/HistoryCard.swift
+++ svea_rike/game_logic/models/HistoryCard.swift
@@ -2,7 +2,7 @@
 //  HistoryCard.swift
 //  svea_rike
 //
-//  Created by Karl Söderberg on 2020-11-24.
+//  Created by Karl Soderberg on 2020-11-24.
 //
 
 import Foundation
@@ -17,12 +17,51 @@ protocol HistoryCardProtocol {
 }
 
 enum HistoryCard: HistoryCardProtocol {
-    case queen1
-    case queen2
-    case queen3
-    case culturePerson1
-    case culturePerson2
-    case culturePerson3
+    case tyska_knektar
+    case maria_elionora
+    case lennart_torstensson
+    case ostindiska_kompaniet
+    case falu_koppargruva
+    case viborgs_fastning
+    case johannes_bureus
+    case gustaf_horn
+    case johan_henric_kellgren
+    case kungliga_slottet
+    case olaus_magnus
+    case haga
+    case karin_mansdotter
+    case hattpartiet
+    case carl_gustaf_armfeldi
+    case sofia_magdalena
+    case ostersjoflotta
+    case carl_von_linne
+    case axel_von_fersen
+    case finska_rytteriet
+    case forbindelser_med_hansan
+    case conrad_von_pyhy
+    case bergsbruk
+    case olaus_petri
+    case drots
+    case orbyhus
+    case jordreform
+    case anders_celsius
+    case skokloster
+    case katarina_jagellonica
+    case vadstena
+    case karoliner
+    case kanslipresident
+    case georg_carl_von_dobeln
+    case emmanuel_swedenborg
+    case tycho_brahe
+    case tons_tacob_berzelius
+    case lovisa_ulrika
+    case glimmingehus
+    case manufakturier
+    case sala_silvergruva
+    case gripsholm
+    case georg_stiernhielm
+    case hedvig_eleonora
+    case carl_michael_bellman
     
     var name: String {
         "history card name"
