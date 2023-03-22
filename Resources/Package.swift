6d6a9074afd4c5453eb79570286be7743597dc70
Karl Söderberg <karl.soderberg@shortcut.io>
2023-03-22 01:14:13 +0100

    fix

--- Resources/Package.swift
+++ Resources/Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Resources",
-            dependencies: [],
-            resources: [.process("assets")]),
+            dependencies: []),
         .testTarget(
             name: "ResourcesTests",
             dependencies: ["Resources"]),
